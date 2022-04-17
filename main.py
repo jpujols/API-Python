8348270befc2150c1ea12d58b3802a37f2f4d7ba
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-17 23:26:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "http://api.open-notify.org/iss-now.json"
+response = requests.get(url)
+response.raise_for_status()
+
+data = response.json()
+
+#Get coordinates
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
